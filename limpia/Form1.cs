cd219c69f7613bed4748e7d7d9ea89d8aaa807f5
marcoescorza117 <marcoescorza117@gmail.com>
2020-01-22 16:02:33 -0600

    Primera Version

---
+++ limpia/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace limpia
+{
+    public partial class Form1 : Form
+    {
+
+        datos dato = new datos();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Creado por: Marco Antonio Castillo Escorza");
+            this.Close();
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dato.detener();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dato.limpiar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dato.iniciar();
+        }
+    }
+}
---
+++ limpia/datos.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace limpia
+{
+    class datos
+    {
+
+        public void detener()
+        {
+
+            ServiceController servicioWindows = new ServiceController("Spooler");
+            try
+            {
+
+                if (servicioWindows != null &&
+                    servicioWindows.Status == ServiceControllerStatus.Running)
+                {
+
+                    servicioWindows.Stop();
+                }
+
+                servicioWindows.WaitForStatus(ServiceControllerStatus.Stopped);
+                servicioWindows.Close();
+                MessageBox.Show("Servicio detenido correctamente");
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error al detener el servicio: " + ex.Message);
+            }
+
+
+        }
+
+
+        public void stop()
+        {
+
+
+
+        }
+
+
+
+        public void limpiar()
+        {
+
+            string ruta = @"C:\WINDOWS\system32\spool\PRINTERS";
+
+            //string ruta = @"C:\test";
+
+            List<string> strFiles = Directory
+                .GetFiles(ruta, "*", SearchOption.AllDirectories)
+                .ToList();
+
+            try
+            {
+
+                //File.Delete(ruta);
+
+                foreach (string fichero in strFiles)
+                {
+
+                    File.Delete(fichero);
+
+                    /*  if (File.Exists(fichero))
+                      {
+
+                          //Console.WriteLine("El archivo sigue existiendo");
+                          MessageBox.Show("Los archivos siguen existiendo");
+                      }
+                      else
+                      {
+
+                          //Console.WriteLine("El archivo YA NO EXISTE");
+                          MessageBox.Show("Los archivos ya no existen");
+                      }
+                      */
+                }
+
+                MessageBox.Show("Archivos eliminados");
+
+
+
+
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show("Error al borrar el archivo {0}", e.ToString());
+            }
+
+
+        }
+
+
+        public void iniciar()
+        {
+            ServiceController servicioWindows = new ServiceController("Spooler");
+            try
+            {
+
+                if (servicioWindows != null &&
+                    servicioWindows.Status != ServiceControllerStatus.Running)
+                {
+
+                    servicioWindows.Start();
+                }
+
+                servicioWindows.WaitForStatus(ServiceControllerStatus.Running);
+                servicioWindows.Close();
+                MessageBox.Show("Servicio iniciado correctamente");
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error al iniciar el servicio: " + ex.Message);
+            }
+
+
+
+        }
+    }
+}
